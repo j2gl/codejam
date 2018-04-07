af019a139620deabd74e0987bb007f3efc9c84ce
Juan J. Garcia (mini) <juanjo.garcia@gmail.com>
2018-04-07 09:11:58 -0600

    Finish SaveUniverse

--- 2018-QR-A-SavingUniverseAgain/src/test/java/SolutionTest.java
+++ 2018-QR-A-SavingUniverseAgain/src/test/java/SolutionTest.java
@@ -4,11 +4,10 @@ import java.io.InputStream;
 
 public class SolutionTest {
 
-
-
     @Test
     public void saveUniverse() {
         final InputStream in = SolutionTest.class.getResourceAsStream("example01.txt");
         Solution.saveUniverse(in);
     }
+
 }
